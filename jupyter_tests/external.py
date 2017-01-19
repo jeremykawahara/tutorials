6cd111156dc187ea3b2ecad0e4b272238309931a
Jeremy Kawahara <jeremy.kawahara@gmail.com>
2017-01-18 21:22:47 -0800

    tutorial using jupyter notebook and code in external files

---
+++ jupyter_tests/external.py
@@ -0,0 +1,13 @@
+# Now what happens if I just change the values in the other notebook...?
+# Copy and paste this block of code into external.py (removing whatever else is in there).
+
+def print_yo():
+    print 'yo'
+
+def print_whasap():
+    print 'whassssap'
+
+def print_the_meaning_of_everything():
+    print '42. But what is the question?'
+    
+# Did you remember to save this?
